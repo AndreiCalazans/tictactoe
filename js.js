fbe2151a58e68944965763286f95af74c86223f3
AndreiCalazans <andreixoc@hotmail.com>
2017-01-08 09:03:03 -0200

    implemented sound effects

--- js.js
+++ js.js
@@ -12,16 +12,10 @@
     //
       // to-dos
 
-        // create level hard(minimax)
-        // make a more atractive UI
-
-
-
+      
+$(".gameOptions").hide();
 // the variables
-var xChosenBox = [] ;
-var oChosenBox = [] ;
 var winningHands = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
-var winningCounter ;
 var boardState = [0,0,0,0,0,0,0,0,0];
 var box = document.getElementsByClassName("box");
 var sign ;
@@ -30,11 +24,17 @@ var computer = -1;
 var game = true;
 var aiLevel ;
 var gameTurn = 0;
+var click =  new Audio;
+click.src = 'click.mp3';
+var tada = new Audio;
+tada.src = 'tada.mp3';
+var pling = new Audio;
+pling.src = 'pling.mp3';
 // types of levels are -easy - medium -hard
 
 
 
-// functions
+// functions ////////////////////////////////////////////////////////////////////
 
 function whoseTurn(first){
   var x = first == "X" ? "X" : "O";
@@ -82,6 +82,7 @@ function claim(click){
   }
   for(var x = 0 ; x <= 8 ; x++){
     if(box[x] === click && boardState[x] == 0){
+
       set(x,user);
       callAI(aiLevel);
 
@@ -91,13 +92,13 @@ function claim(click){
 }
 ////////////////////////////////////////
 function set(index, player){
-  if(player == computer){
-
-  }
 
   var symbol= player == user ? sign : signComputer;
   box[index].innerHTML = symbol;
+  box[index].style.fontSize = "8vw"
   boardState[index] = player;
+  click.currentTime = 0;
+  click.play();
   checkWin(player, boardState);
   boardFull();
 }
@@ -270,156 +271,42 @@ function callAI(aiLevel){
       mediumAI();
   }
 }
-
+///////////////////////////////////////////////////////////////
 
 function reset(){
   for(var x = 0 ; x <= 8 ; x++){
     boardState[x] = 0;
     box[x].innerHTML = "";
+    box[x].style.fontSize = "0px"
   }
 }
 
-//UI functions above
-function intro(){
-  document.querySelector(".gameOptions").classList.toggle("display");
+//UI functions below/////////////////////////////////////////////////////////////////////
+function intro(msg){
+  if(msg == undefined){
+
+  }else{
+   document.querySelector("#newWindow p").textContent = msg;
+  }
+  $(".gameOptions").toggle('slow');
+
+//  document.querySelector(".gameOptions").classList.toggle("display");
 }
 
 function continuing(situation, player){
   var opositeSign = sign == "X" ? "O" : "X";
-  player  = player == 1 ? sign : opositeSign;
+  player  = player == 1 ? "You" : "Computer";
 var msg;
   if(situation == "win"){
     msg = `${player} Wins!!!`
+    tada.currentTime = 0;
+    tada.play();
   }else {
+    pling.currentTime = 0;
+    pling.play();
     msg = `It's a Tie`
   }
 
-    document.querySelector(".gameOptions").classList.toggle("display");
-    document.getElementById('display').innerHTML = `<div id = "newWindow">
-        <p>${msg}</p>
-      <div>
-        <form action="">
-          <input type="radio" name="symbol" value='X'> X
-          <input type="radio" name="symbol" value='O'> O
-        </form>
+  intro(msg);
 
-        <select name="level" id="level">
-            <option value="easy">easy</option>
-            <option value="medium">medium</option>
-            <option value="hard">hard</option>
-        </select>
-        <button onclick="gameStart()">Start</button>
-      </div>
-    </div>`
 }
-
-
-
-
-
-// the commands
-
-
-
-
-
-
-
-
-/*
-// function to check if won it look in the array of the selected boxes and looks for matches in t
-function checkIfWon(chosenBox,winner){
-  winningCounter = 0 ;
-
-  for(var i = 0 ; i < winningHands.length ; i++){
-    // iterate through the selected optiions and see if we can get a match from the winning hands
- chosenBox.forEach(function(e){
-
-   if(boardSquares.length === 9){
-     document.querySelector(".gameOptions").classList.add("display");
-     document.getElementById("display").innerHTML = "<div id='newWindow'><p>It is a Tie !!!</p><p>Play again</p><div><button class='X' onClick=gameStart('X')>X</button><button class='O' onClick=gameStart('O')>O</button></div></div>"
-   }
-   if(winningHands[i].indexOf(Number(e)) != -1 ){
-     winningCounter ++;
-     if (winningCounter == 3){
-       document.querySelector(".gameOptions").classList.add("display");
-       document.getElementById("display").innerHTML = "<div id='newWindow'><p>"+winner+" Won</p><p>Play again</p><div><button class='X' onClick=gameStart('X')>X</button><button class='O' onClick=gameStart('O')>O</button></div></div>"
-        return "winner";
-     } // in case of a winner add this to the html
-   }
-
- })
-     winningCounter = 0;  // reset it
-  } // end of the for loop
-}
-
-function intro(){
-  document.querySelector(".gameOptions").classList.toggle("display");
-}
-
-
-function gameStart(a){
- // these commands are resetting the board.
-  winningCounter = 0;
-  xChosenBox = [] ;
-  oChosenBox = [];
-  sign = a;
-  boardSquares = [];
-document.querySelector('#turnTable').innerHTML = `It is ${sign}'s turn`;
-gameBoard.addEventListener('click', function(){
-  document.querySelector('#turnTable').innerHTML = `It is ${sign}'s turn`;
-
-})
-
-box.forEach(function(e){
-  e.innerHTML ="";
-})
-//resets finish here ^
-  document.querySelector(".gameOptions").classList.remove("display");
-
-} // end of gameStart
-// it is listening to the events and acting according the user. if x or o.
-box.forEach(function(e){
-
-  e.addEventListener("click", function(){
-
-
-
-    if(e.innerHTML == ""){ // prevents it from clicking in the same place
-       if (sign == "X"){
-         xChosenBox.push(e.id);
-         e.innerHTML = sign;
-         boardSquares.push(sign);
-        checkIfWon(xChosenBox , "X");
-        iterator(sign);
-      }else if (sign == "O"){
-        oChosenBox.push(e.id);
-        e.innerHTML = sign;
-        boardSquares.push(sign);
-       checkIfWon(oChosenBox , "O");
-       iterator(sign);
-      }
-  }
-  })
-})
-
-// make a function to iterate through x and o
-function iterator(signal){
-
-  if(signal == "X"){
-    sign = "O"
-    return sign
-  } else if (signal == "O"){
-    sign = "X";
-    return sign
-  }
-}
-// make a universal function to take in x or o and insert it on the board while it also pushes the value to the array and
-// checks to see if the board square has already been filled ...
-
-// algorithm to find out a winnner
-*/
-
-
-
-// make a OOprogramming
